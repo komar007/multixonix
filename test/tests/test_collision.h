babeec53c1ad5c23ed195a26d71f013d7591f8d9
Michal Trybus <komar007@gmail.com>
2011-04-22 16:47:14 +0200

    [test] added some collision tests

--- test/tests/test_collision.h
+++ test/tests/test_collision.h
@@ -3,9 +3,21 @@
 #include "collision.h"
 
 class CollisionTestSuite : public TestSuite {
+private:
+	const Point la, lb;
+	const Point a, b, c, d, e, f, g;
 public:
 	CollisionTestSuite()
 		: TestSuite("Collision")
+		, la(3, 2)
+		, lb(6, 5)
+		, a(1, 1)
+		, b(2, 2)
+		, c(4, 1)
+		, d(4, 2)
+		, e(4, 3)
+		, f(5, 3)
+		, g(4, 4)
 	{
 		add_test("line_segment_collision",
 				(test_fun)&CollisionTestSuite::test_line_segment_collision);
@@ -13,12 +25,14 @@ public:
 				(test_fun)&CollisionTestSuite::test_line_segment_collision_degen);
 		add_test("segment_segment_collision",
 				(test_fun)&CollisionTestSuite::test_segment_segment_collision);
+		add_test("segment_segment_collision_degenerated",
+				(test_fun)&CollisionTestSuite::test_segment_segment_collision_degen);
+		add_test("segment_segment_collision_colinear",
+				(test_fun)&CollisionTestSuite::test_segment_segment_collision_colin);
 	}
 
 	void test_line_segment_collision()
 	{
-		Point la(3, 2);
-		Point lb(6, 5);
 		assert_eq(line_segment_collision(la, lb, Point(8, 4), Point(8, 6)), false);
 		assert_eq(line_segment_collision(la, lb, Point(8, 4), Point(8, 7)), false);
 		assert_eq(line_segment_collision(la, lb, Point(8, 4), Point(8, 8)), true);
@@ -28,8 +42,6 @@ public:
 	}
 	void test_line_segment_collision_degen()
 	{
-		Point la(3, 2);
-		Point lb(6, 5);
 		assert_eq(line_segment_collision(la, lb, la, la), true);
 		assert_eq(line_segment_collision(la, lb, la, lb), true);
 		assert_eq(line_segment_collision(la, lb, la, Point(2, 4)), true);
@@ -37,8 +49,19 @@ public:
 	}
 	void test_segment_segment_collision()
 	{
-		Point a(1, 1), b(2, 2);
 		assert_eq(segment_segment_collision(a, b, Point(2, 1), Point(1, 2)), true);
 		assert_eq(segment_segment_collision(a, b, Point(2, 1), Point(1.5, 1.5)), false);
+	}
+	void test_segment_segment_collision_degen()
+	{
+		assert_eq(segment_segment_collision(c, d, d, f), false);
+		assert_eq(segment_segment_collision(c, d, d, e), false);
+	}
+	void test_segment_segment_collision_colin()
+	{
+		assert_eq(segment_segment_collision(c, d, e, g), false);
+		assert_eq(segment_segment_collision(c, d, g, e), false);
+		assert_eq(segment_segment_collision(e, g, c, d), false);
+		assert_eq(segment_segment_collision(e, g, d, c), false);
 	}
 };
