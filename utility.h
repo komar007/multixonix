015790cd47e63993950308f4f11def18f20570b4
Michal Trybus <komar007@gmail.com>
2011-04-30 20:54:41 +0200

    [utility] fixed error in cyclic_iterator

--- utility.h
+++ utility.h
@@ -15,17 +15,21 @@ private:
 	It it_beg;
 	It it_end;
 	It i;
+	short cycles;
 public:
 	cyclic_iterator(It beg, It end, It start)
 		: it_beg(beg)
 		, it_end(end)
 		, i(start)
+		, cycles(0)
 	{
 	}
 	cyclic_iterator& operator++()
 	{
-		if (++i == it_end)
+		if (++i == it_end) {
 			i = it_beg;
+			++cycles;
+		}
 		return *this;
 	}
 	auto operator*() const -> decltype(*i)&
@@ -35,11 +39,25 @@ public:
 	bool operator==(const cyclic_iterator<It>& o) const
 	{
 		return  i == o.i ||
-			(o.i == it_end && i == it_beg) ||	// o is end
-			(i == o.it_end && o.i == o.it_beg);	// *this is end
+			(o.i == it_end && i == it_beg && cycles > 0) ||    	// o is end
+			(i == o.it_end && o.i == o.it_beg && o.cycles > 0);	// *this is end
 	}
 	bool operator!=(const cyclic_iterator<It>& o) const
 	{
 		return !operator==(o);
+	}
+
+	//! Iterator move. Does not support cycling!
+	cyclic_iterator operator+(size_t o) const
+	{
+		return cyclic_iterator(it_beg, it_end, i+o);
+	}
+};
+
+template <typename It>
+struct dereference {
+	auto operator()(const It& it) -> decltype(*it)
+	{
+		return *it;
 	}
 };
