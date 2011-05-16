459aaa5123a326d3589068e8ab168e54e2808229
Michal Trybus <komar007@gmail.com>
2011-05-16 18:33:58 +0200

    [shapemanager] added remaining notifications
    added observer handler

--- shapemanager.cpp
+++ shapemanager.cpp
@@ -104,12 +104,12 @@ ShapeMessage::ShapeMessage(const ShapeMessage& o)
 	: type(o.type)
 	, id(o.id)
 	, info(NULL)
-	, point(NULL)
+	, extension_point(NULL)
 {
 	if (o.info)
 		info = new ShapeCreationInfo(*o.info);
-	if (o.point)
-		point = new Point(*o.point);
+	if (o.extension_point)
+		extension_point = new Point(*o.extension_point);
 }
 const ShapeMessage& ShapeMessage::operator=(const ShapeMessage& o)
 {
@@ -121,11 +121,11 @@ const ShapeMessage& ShapeMessage::operator=(const ShapeMessage& o)
 		else
 			info = new ShapeCreationInfo(*o.info);
 	}
-	if (o.point) {
-		if (point)
-			*point = *o.point;
+	if (o.extension_point) {
+		if (extension_point)
+			*extension_point = *o.extension_point;
 		else
-			point = new Point(*o.point);
+			extension_point = new Point(*o.extension_point);
 	}
 	return *this;
 }
@@ -133,33 +133,42 @@ ShapeMessage::ShapeMessage(ShapeMessageType _type, int _id, const ShapeCreationI
 	: type(_type)
 	, id(_id)
 	, info(NULL)
-	, point(NULL)
+	, extension_point(NULL)
 {
 	if (type != CREATED)
 		throw domain_error("type != CREATED in creation message");
 	info = new ShapeCreationInfo(_info);
 }
-ShapeMessage::ShapeMessage(ShapeMessageType _type, int _id, const Point& _point)
+ShapeMessage::ShapeMessage(ShapeMessageType _type, int _id, const Point& _extension_point)
 	: type(_type)
 	, id(_id)
 	, info(NULL)
-	, point(NULL)
+	, extension_point(NULL)
 {
 	if (type != EXTENDED)
 		throw domain_error("type != EXTENDED in extension message");
-	point = new Point(_point);
+	extension_point = new Point(_extension_point);
+}
+ShapeMessage::ShapeMessage(ShapeMessageType _type, int _id)
+	: type(_type)
+	, id(_id)
+	, info(NULL)
+	, extension_point(NULL)
+{
+	if (type != DESTROYED)
+		throw domain_error("type != DESTROYED in destruction message");
 }
 ShapeMessage::ShapeMessage()
 	: type(DUMMY)
 	, id(-1)
 	, info(NULL)
-	, point(NULL)
+	, extension_point(NULL)
 {
 }
 ShapeMessage::~ShapeMessage()
 {
 	delete info;
-	delete point;
+	delete extension_point;
 }
 
 ShapeManager::ShapeManager(bool _with_detector)
@@ -172,75 +181,129 @@ const Shape& ShapeManager::get_shape_const_ref(int id) const throw (out_of_range
 {
 	auto trace_it = shapes.find(id);
 	if (trace_it == shapes.end())
-		throw out_of_range("no such trace in ShapeManager");
+		throw out_of_range("no such shape in ShapeManager");
 	return *trace_it->second;
 }
 Shape& ShapeManager::get_shape_ref(int id) throw (out_of_range)
 {
 	auto trace_it = shapes.find(id);
 	if (trace_it == shapes.end())
-		throw out_of_range("no such trace in ShapeManager");
+		throw out_of_range("no such shape in ShapeManager");
 	return *trace_it->second;
 }
 
-int ShapeManager::add_shape(const Path& path)
+int ShapeManager::add_shape_impl(const Path& path, int id)
 {
 	Shape *sh = new Shape(path, with_detector);
-	++last_id;
-	shapes.insert(make_pair(last_id, sh));
-	notify(ShapeMessage(CREATED, last_id, ShapeCreationInfo(path)));
-	return last_id;
+	if (id == -1) {
+		shapes.insert(make_pair(++last_id, sh));
+		return last_id;
+	} else {
+		shapes.insert(make_pair(id, sh));
+		return id;
+	}
+}
+int ShapeManager::add_shape(const Path& path)
+{
+	int id = add_shape_impl(path);
+	notify(ShapeMessage(CREATED, id, ShapeCreationInfo(path)));
+	return id;
 }
 int ShapeManager::start_trace(const Point& point)
 {
 	Path p(false);
 	p.push_back(point);
-	return add_shape(p);
+	int id = add_shape(p);
+	notify(ShapeMessage(CREATED, last_id, ShapeCreationInfo(p)));
+	return id;
+}
+void ShapeManager::extend_trace_impl(int id, const Point& point)
+{
+	Shape& shape = get_shape_ref(id);
+	shape.extend(point);
 }
 void ShapeManager::extend_trace(int id, const Point& point)
 {
-	Shape& shape = get_shape_ref(id);
-	try {
-		shape.extend(point);
-	} catch (domain_error) {
-		throw domain_error("closed path as trace in ShapeManager::extend_trace");
-	}
+	extend_trace_impl(id, point);
 	notify(ShapeMessage(EXTENDED, id, point));
 }
-int ShapeManager::make_cut_shape_forward(const Path& trace, const Path& shape, int s1, int s2)
+int ShapeManager::cut_shape_impl(const Path& trace, const Path& shape, int s1, int s2, ShapeDirection dir, int id)
 {
 	Path p(trace);
 	p.closed = true;
-	Path::const_iterator it_end1 = s1 != s2 ?
-		shape.nth_point(s1+1) :
-		shape.nth_point(s1+1).next_cycle();
-	for (Path::const_iterator it = shape.nth_point(s2+1); it != it_end1; ++it)
-		p.push_back(*it);
+	switch (dir) {
+	case FORWARD: {
+		Path::const_iterator it_end = s1 != s2 ?
+			shape.nth_point(s1+1) :
+			shape.nth_point(s1+1).next_cycle();
+		for (Path::const_iterator it = shape.nth_point(s2+1); it != it_end; ++it)
+			p.push_back(*it);
+		break; }
+	case REVERSE: {
+		Path::const_reverse_iterator it_end = shape.nth_point(s1);
+		for (Path::const_reverse_iterator it = shape.nth_point(s2); it != it_end; ++it)
+			p.push_back(*it);
+		break; }
+	}
 	Shape *new_shape = new Shape(move(p), with_detector);
-	shapes.insert(make_pair(++last_id, new_shape));
-	return last_id;
-}
-int ShapeManager::make_cut_shape_reverse(const Path& trace, const Path& shape, int s1, int s2)
-{
-	Path p(trace);
-	p.closed = true;
-	Path::const_reverse_iterator it_end2 = shape.nth_point(s1);
-	for (Path::const_reverse_iterator it = shape.nth_point(s2); it != it_end2; ++it)
-		p.push_back(*it);
-	Shape *new_shape = new Shape(move(p), with_detector);
-	shapes.insert(make_pair(++last_id, new_shape));
-	return last_id;
+	if (id == -1) {
+		shapes.insert(make_pair(++last_id, new_shape));
+		return last_id;
+	} else {
+		shapes.insert(make_pair(id, new_shape));
+		return id;
+	}
 }
 pair<int, int> ShapeManager::cut_shape(int trace_id, int id, int s1, int s2)
 {
 	const Path& trace = get_shape_ref(trace_id).get_path();
 	const Path& shape = get_shape_ref(id).get_path();
 	pair<int, int> ids(-1, -1);
-	ids.first  = make_cut_shape_forward(trace, shape, s1, s2),
+	ids.first  = cut_shape_impl(trace, shape, s1, s2, FORWARD),
 	notify(ShapeMessage(CREATED, ids.first,
 				ShapeCreationInfo(trace_id, id, s2, s1, FORWARD)));
-	ids.second = make_cut_shape_reverse(trace, shape, s1, s2);
+	ids.second = cut_shape_impl(trace, shape, s1, s2, REVERSE);
 	notify(ShapeMessage(CREATED, ids.second,
 				ShapeCreationInfo(trace_id, id, s2, s1, REVERSE)));
 	return ids;
+}
+void ShapeManager::destroy_shape_impl(int id)
+{
+	auto shape_it = shapes.find(id);
+	if (shape_it == shapes.end())
+		throw out_of_range("No such shape in destroy_shape");
+	shapes.erase(shape_it);
+}
+void ShapeManager:: destroy_shape(int id)
+{
+	destroy_shape_impl(id);
+	notify(ShapeMessage(DESTROYED, id));
+}
+
+void ShapeManager::update(const Observable<ShapeMessage>& obj, const ShapeMessage& msg)
+{
+	switch (msg.type) {
+	case CREATED:
+		if (msg.info->path) {
+			add_shape_impl(*msg.info->path, msg.id);
+		} else {
+			const Path& trace = get_shape_const_ref(msg.info->trace_id).get_path();
+			const Path& shape = get_shape_const_ref(msg.info->shape_id).get_path();
+			cut_shape_impl(
+				trace, shape,
+				msg.info->shape_start, msg.info->shape_end,
+				msg.info->shape_dir,
+				msg.id
+			);
+		}
+		break;
+	case DESTROYED:
+		destroy_shape_impl(msg.id);
+		break;
+	case EXTENDED:
+		extend_trace_impl(msg.id, *msg.extension_point);
+		break;
+	case DUMMY:
+		break;
+	}
 }
--- shapemanager.h
+++ shapemanager.h
@@ -49,29 +49,34 @@ public:
 	ShapeMessageType type;
 	int id;
 	ShapeCreationInfo* info;
-	Point *point;
-	ShapeMessage(ShapeMessageType _type, int _id, const ShapeCreationInfo& _info = ShapeCreationInfo());
-	ShapeMessage(ShapeMessageType _type, int _id, const Point& _point);
+	Point *extension_point;
+	ShapeMessage(ShapeMessageType _type, int _id, const ShapeCreationInfo& _info);
+	ShapeMessage(ShapeMessageType _type, int _id, const Point& _extension_point);
+	ShapeMessage(ShapeMessageType _type, int _id);
 	ShapeMessage();
 	ShapeMessage(const ShapeMessage& o);
 	const ShapeMessage& operator=(const ShapeMessage& o);
 	~ShapeMessage();
 };
 
-class ShapeManager : public Observable<ShapeMessage> {
+class ShapeManager : public Observable<ShapeMessage>, public Observer<ShapeMessage> {
 private:
 	bool with_detector;
 	std::unordered_map<int, Shape*> shapes;
 	int last_id;
 
-	int make_cut_shape_forward(const Path& trace, const Path& shape, int s1, int s2);
-	int make_cut_shape_reverse(const Path& trace, const Path& shape, int s1, int s2);
+	int cut_shape_impl(const Path& trace, const Path& shape, int s1, int s2, ShapeDirection dir, int id = -1);
+	int add_shape_impl(const Path& path, int id = -1);
+	void extend_trace_impl(int id, const Point& point);
+	void destroy_shape_impl(int id);
 	Shape& get_shape_ref(int id) throw (std::out_of_range);
 public:
+	virtual void update(const Observable<ShapeMessage>& obj, const ShapeMessage& msg);
 	const Shape& get_shape_const_ref(int id) const throw (std::out_of_range);
 	ShapeManager(bool _with_detector);
 	int add_shape(const Path& path);
 	int start_trace(const Point& point);
-	void extend_trace(int, const Point& point);
+	void extend_trace(int id, const Point& point);
 	std::pair<int, int> cut_shape(int trace_id, int id, int s1, int s2);
+	void destroy_shape(int id);
 };
